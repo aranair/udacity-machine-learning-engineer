5ba7b90269deb508fbf510fdc60d26003ff2f894
Boa Ho man <boa.homan@gmail.com>
2016-01-30 11:49:33 +0800

    first 3 parts

--- boston_housing.py
+++ boston_housing.py
@@ -26,8 +26,6 @@ def explore_city_data(city_data):
     # Get the labels and features from the housing data
     housing_prices = city_data.target
     housing_features = city_data.data
-    # print housing_prices
-    # print housing_features
 
     ###################################
     ### Step 1. YOUR CODE GOES HERE ###
@@ -197,11 +195,11 @@ def fit_predict_model(city_data):
 
     # Fit the learner to the training data to obtain the best parameter set
     print "Final Model: "
-    print reg.fit(X, y)
+    print regressor.fit(X, y)
 
     # Use the model to predict the output of a particular sample
     x = [11.95, 0.00, 18.100, 0, 0.6590, 5.6090, 90.00, 1.385, 24, 680.0, 20.20, 332.09, 12.13]
-    y = reg.predict(x)
+    y = regressor.predict(x)
     print "House: " + str(x)
     print "Prediction: " + str(y)
 
